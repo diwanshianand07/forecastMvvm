2b754730ca6ebe3742c9ae8a98d1617f5f70770d
Diwanshi <diwanshi.anand@affordplan.com>
2022-09-05 13:07:49 +0530

    first commit

---
+++ app/src/main/java/com/example/forecastmvvm/ui/weather/Future/list/FutureListWeatherViewModel.kt
@@ -0,0 +1,7 @@
+package com.example.forecastmvvm.ui.weather.Future.list
+
+import androidx.lifecycle.ViewModel
+
+class FutureListWeatherViewModel : ViewModel() {
+    // TODO: Implement the ViewModel
+}
